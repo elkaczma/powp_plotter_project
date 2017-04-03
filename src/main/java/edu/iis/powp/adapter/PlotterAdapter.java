e28c129cdd65a13985dc356f17fe6ca31f58738c
Aleksander Kaczmarczyk <axelkko@gmail.com>
2017-04-03 11:39:21 +0200

    3.2.1: poprawa nazwy klasy adaptera

--- src/main/java/edu/iis/powp/adapter/PlotterAdapter.java
+++ src/main/java/edu/iis/powp/adapter/PlotterAdapter.java
@@ -11,12 +11,12 @@ import edu.kis.powp.drawer.shape.LineFactory;
 /**
  * Plotter adapter to drawer with several bugs. 
  */
-public class MyAdapter implements IPlotter
+public class PlotterAdapter implements IPlotter
 { 
 	private int startX = 0, startY = 0;
 	private DrawPanelController drawPanelController;
 	
-    public MyAdapter(DrawPanelController drawPanelController) {
+    public PlotterAdapter(DrawPanelController drawPanelController) {
     	this.drawPanelController = drawPanelController;
 	}
     
