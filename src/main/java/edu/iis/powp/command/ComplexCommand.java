3fa3c9880342f138c84b28f52b07eabe8adab10c
Aleksander Kaczmarczyk <194972@edu.p.lodz.pl>
2017-04-27 14:52:32 +0200

    lab4 3.1.2: ComplexCommand

---
+++ src/main/java/edu/iis/powp/command/ComplexCommand.java
@@ -0,0 +1,23 @@
+package edu.iis.powp.command;
+
+import java.util.List;
+
+import edu.iis.client.plottermagic.IPlotter;
+
+public class ComplexCommand implements PlotterCommand {
+	
+	private List<PlotterCommand> commandsList;
+	
+	public ComplexCommand(List<PlotterCommand> commandsList) {
+		super();
+		this.commandsList = commandsList;
+	}
+
+	@Override
+	public void execute(IPlotter plotter) {
+		for (PlotterCommand command : this.commandsList) {
+			command.execute(plotter);
+		}
+	}
+
+}
